5440a6a061a24b4d577f355383ebc501c887e2a9
和田健嗣 <ponporopen@gmail.com>
2020-05-11 19:53:02 +0900

    github actions test

--- app/next.config.js
+++ app/next.config.js
@@ -2,10 +2,7 @@ console.log(process.env)
 
 const env = process.env.NODE_ENV !== 'production' ?
   {...require('dotenv').config().parsed} :
-  {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
-  }
+  {...process.env.SECRETS}
 
 module.exports = {
   distDir: '../functions/dist',
