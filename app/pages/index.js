71dc49f2e6338644e7b091a39f9f9fa6c9c5bc18
和田健嗣 <ponporopen@gmail.com>
2020-05-11 17:29:04 +0900

    github actions test

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,18 +1,19 @@
 import React, {useState, useEffect} from 'react'
 import {createClient} from "contentful"
 
-const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-})
+// const client = createClient({
+//   space: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+// })
 
 const Index = () => {
   const [entries, setEntries] = useState([])
   useEffect(async () => {
-    setEntries(await client.getEntries())
+    // setEntries(await client.getEntries())
   }, [])
   return (
     <>
+      <div>actions test</div>
       {entries.items?.map(i => (
         <div key={i.sys.id}>
           {i.sys.id}
