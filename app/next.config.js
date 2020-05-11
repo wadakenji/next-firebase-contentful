261787fdb122fa98e7c36c5cf8f52f08b3c155bb
和田健嗣 <ponporopen@gmail.com>
2020-05-11 18:30:49 +0900

    github actions test

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,8 @@
-const dotenvFilename = process.env.NODE_ENV !== 'production' ? '.env.dev' : '.env.prod'
+const env = process.env.NODE_ENV !== 'production' ?
+  {...require('dotenv').config().parsed} :
+  {...process.env}
 
 module.exports = {
   distDir: '../functions/dist',
-  env: {...require('dotenv').config({path: dotenvFilename}).parsed},
+  env
 }
